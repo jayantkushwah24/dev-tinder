28950fb7c3949bff013a578cd77ba4416e50e69e
Jayant Kushwah <jayantkushwah8@gmail.com>
2025-06-11 12:41:53 +0000

    init db

---
+++ model/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import express from "express";
+import { connectDb } from "../config/database.js";
+import User from "../model/user.js";
 const app = express();
 const PORT = 7777;
 
-app.get("/", (req, res) => {
-  res.send("welcome to dashboard");
+app.post("/signup", async (req, res, next) => {
+  //creating a new instance of a model
+  const user = new User({
+    firstName: "jayant",
+    lastName: "kushwah",
+    email: "jk@gmail.com",
+    password: "zxysj",
+  });
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (error) {
+    res.status(400).send("error adding the user" + error.message);
+  }
 });
 
-app.listen(PORT, () => {
-  console.log("server is listening on port", PORT);
-});
+connectDb()
+  .then(() => {
+    console.log("database is successfully connected");
+    app.listen(PORT, () => {
+      console.log("server is listening on port", PORT);
+    });
+  })
+  .catch((error) => console.log("error"));
