3bbdec5e02ff9ab47fccc9f15586c8c5427208e6
Jayant Kushwah <jayantkushwah8@gmail.com>
2025-07-25 15:32:46 +0530

    updated password

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDb = async () => {
-  await mongoose.connect(process.env.CONNECTION_STRING);
+  await mongoose.connect("mongodb+srv://jayantkushwah8:Kvoq0xH8Atkla7nP@cluster0.edmb6db.mongodb.net/devTinder");
 };
